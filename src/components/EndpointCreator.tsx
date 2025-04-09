a015373aab50ac26ae9f0762079db282ee326258
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-09 14:28:41 +0900

    ResponseType 컴포넌트 적용

--- src/components/EndpointCreator.tsx
+++ src/components/EndpointCreator.tsx
@@ -2,13 +2,18 @@
 
 import { EndpointPath } from "@/components/EndpointCreatorAtoms/EndpointPath";
 import { HttpMethod } from "@/components/EndpointCreatorAtoms/HttpMethod";
+import { ResponseType } from "@/components/EndpointCreatorAtoms/ResponseType";
 
 export const EndpointCreator = () => {
   return (
-    <div>
+    <div className="space-y-4">
       <div className="flex gap-8">
         <EndpointPath />
         <HttpMethod />
+      </div>
+
+      <div>
+        <ResponseType />
       </div>
     </div>
   );
---
+++ src/components/EndpointCreatorAtoms/ResponseType.tsx
@@ -0,0 +1,29 @@
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from "@/components/ui/select";
+
+export const ResponseType = () => {
+  return (
+    <div className="space-y-2">
+      <Label htmlFor="response-type">Resopnse Type</Label>
+      <Select>
+        <SelectTrigger
+          id="response-type"
+          className="w-[500px]"
+        >
+          <SelectValue placeholder="Select response type" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="object">Object</SelectItem>
+          <SelectItem value="array">Array</SelectItem>
+          <SelectItem value="primitive">Primitive</SelectItem>
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
