bf3ce272807359277e0e69e49ed1f487d4556257
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-09 23:47:02 +0900

    Supabase DB 연결 함수 추가

--- src/components/EndpointCreator/AddEndpointButton.tsx
+++ src/components/EndpointCreator/AddEndpointButton.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/libs/supabase/clientClient";
 import {
+  useAppInitializerStore,
   useEndpointStore,
   useHttpStore,
   useResponseStore,
@@ -13,6 +15,7 @@ import { useEffect, useState } from "react";
 export const AddEndpointButton = () => {
   const [pathStartWordAlert, setPathStartWordAlert] = useState(false);
 
+  const { userId } = useAppInitializerStore();
   const { endpointPath, httpMethod } = useHttpStore();
   const { successStatus, errorStatus } = useSuccessOrErrorStore();
   const { successResponse, errorResponse } = useResponseStore();
@@ -26,11 +29,35 @@ export const AddEndpointButton = () => {
     }
   }, [endpointPath]);
 
+  const insertData = async () => {
+    try {
+      const supabase = createClient();
+
+      if (!userId) throw new Error("nanoId 없음");
+
+      const payload = {
+        nanoid: userId,
+        path: endpointPath,
+        method: httpMethod,
+        status_success: successStatus,
+        status_error: errorStatus,
+        response_success: successResponse,
+        response_error: errorResponse
+      };
+
+      const { error } = await supabase.from("endpoints").insert(payload);
+
+      if (error) throw new Error(error.message);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <Button
       disabled={pathStartWordAlert}
       className="w-full"
-      onClick={() =>
+      onClick={async () => {
         addEndpoint({
           endpointPath,
           httpMethod,
@@ -38,8 +65,9 @@ export const AddEndpointButton = () => {
           errorStatus,
           successResponse,
           errorResponse
-        })
-      }
+        });
+        await insertData();
+      }}
     >
       Add Endpoint
     </Button>
