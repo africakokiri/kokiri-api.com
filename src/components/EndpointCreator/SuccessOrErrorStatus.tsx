af396e1d374ccb420016e3a3580c7bced1e36c94
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-09 20:27:54 +0900

    SuccessOrErrorStatus 컴포넌트 zustand 동기화

--- src/components/EndpointCreator/SuccessOrErrorStatus.tsx
+++ src/components/EndpointCreator/SuccessOrErrorStatus.tsx
@@ -2,17 +2,26 @@
 
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useSuccessOrErrorStore } from "@/libs/zustand/store";
 
 export const SuccessOrErrorStatus = ({
   status
 }: {
   status: "Success" | "Error";
 }) => {
+  const { successStatus, setSuccessStatus, errorStatus, setErrorStatus } =
+    useSuccessOrErrorStore();
+
   return (
     <div>
       <Label htmlFor={`${status} Status`}>{`${status} Status`}</Label>
       <Input
-        defaultValue={status === "Success" ? "200" : "400"}
+        defaultValue={status === "Success" ? successStatus : errorStatus}
+        onChange={(e) =>
+          status === "Success"
+            ? setSuccessStatus(e.target.value)
+            : setErrorStatus(e.target.value)
+        }
         id={`${status} Status`}
       />
     </div>
