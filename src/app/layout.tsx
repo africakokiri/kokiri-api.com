e0f0002da495dd5c73e8f71f6b816177dcf2c93c
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-09 12:43:13 +0900

    Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,11 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: ""
+  title: "REST API Mock Server",
+  description: "Mock REST API server for frontend developers",
+  icons: {
+    icon: "/icons/kokiri.svg"
+  }
 };
 
 export default function RootLayout({
@@ -12,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="en">
       <body className="antialiased">{children}</body>
     </html>
   );
