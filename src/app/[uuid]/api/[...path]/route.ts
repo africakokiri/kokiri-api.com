1323c2fc3d5587787d1a68c88c598aaeb7840b87
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-11 12:04:57 +0900

    Route 관련 에러 수정하기 전 임시 저장

---
+++ src/app/[uuid]/api/[...path]/route.ts
@@ -0,0 +1,42 @@
+import { createClient } from "@/libs/supabase/serverClient";
+
+import { type NextRequest, NextResponse } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { uuid: string; path: string[] } }
+) {
+  try {
+    const supabase = await createClient();
+
+    const uuid = params?.uuid;
+    const pathArray = params?.path || [];
+    const fullPath = "/api/" + pathArray.join("/");
+    const method = "GET";
+
+    const { data, error } = await supabase
+      .from("endpoints")
+      .select("*")
+      .eq("uuid", uuid)
+      .eq("path", fullPath)
+      .eq("method", method)
+      .maybeSingle();
+
+    if (error || !data) {
+      return NextResponse.json(
+        { message: "API not found or error", error },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(JSON.parse(data.response_success), {
+      status: parseInt(data.status_success) || 200
+    });
+  } catch (err) {
+    console.error("Error processing request:", err);
+    return NextResponse.json(
+      { message: "Internal server error", error: err },
+      { status: 500 }
+    );
+  }
+}
