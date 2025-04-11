9b21a53408afe10c45ab6ad0b4b7265eff87894b
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-11 12:12:08 +0900

    Route 파일 POST, PUT, PATCH, DELETE method 함수 추가

--- src/app/[uuid]/api/[...path]/route.ts
+++ src/app/[uuid]/api/[...path]/route.ts
@@ -2,42 +2,69 @@ import { createClient } from "@/libs/supabase/serverClient";
 
 import { type NextRequest, NextResponse } from "next/server";
 
+async function handleRequest(
+  req: NextRequest,
+  method: string,
+  props: { params: Promise<{ uuid: string; path: string[] }> }
+) {
+  const params = await props.params;
+  const supabase = await createClient();
+
+  const uuid = params?.uuid;
+  const pathArray = params?.path || [];
+  const fullPath = "/api/" + pathArray.join("/");
+
+  const { data, error } = await supabase
+    .from("endpoints")
+    .select("*")
+    .eq("uuid", uuid)
+    .eq("path", fullPath)
+    .eq("method", method)
+    .maybeSingle();
+
+  if (error || !data) {
+    return NextResponse.json(
+      { message: "API not found or error", error },
+      { status: 404 }
+    );
+  }
+
+  return NextResponse.json(JSON.parse(data.response_success), {
+    status: parseInt(data.status_success) || 200
+  });
+}
+
 export async function GET(
   req: NextRequest,
   props: { params: Promise<{ uuid: string; path: string[] }> }
 ) {
-  const params = await props.params;
-  try {
-    const supabase = await createClient();
+  return handleRequest(req, "GET", props);
+}
 
-    const uuid = params?.uuid;
-    const pathArray = params?.path || [];
-    const fullPath = "/api/" + pathArray.join("/");
-    const method = "GET";
+export async function POST(
+  req: NextRequest,
+  props: { params: Promise<{ uuid: string; path: string[] }> }
+) {
+  return handleRequest(req, "POST", props);
+}
 
-    const { data, error } = await supabase
-      .from("endpoints")
-      .select("*")
-      .eq("uuid", uuid)
-      .eq("path", fullPath)
-      .eq("method", method)
-      .maybeSingle();
+export async function PUT(
+  req: NextRequest,
+  props: { params: Promise<{ uuid: string; path: string[] }> }
+) {
+  return handleRequest(req, "PUT", props);
+}
 
-    if (error || !data) {
-      return NextResponse.json(
-        { message: "API not found or error", error },
-        { status: 404 }
-      );
-    }
+export async function PATCH(
+  req: NextRequest,
+  props: { params: Promise<{ uuid: string; path: string[] }> }
+) {
+  return handleRequest(req, "PATCH", props);
+}
 
-    return NextResponse.json(JSON.parse(data.response_success), {
-      status: parseInt(data.status_success) || 200
-    });
-  } catch (err) {
-    console.error("Error processing request:", err);
-    return NextResponse.json(
-      { message: "Internal server error", error: err },
-      { status: 500 }
-    );
-  }
+export async function DELETE(
+  req: NextRequest,
+  props: { params: Promise<{ uuid: string; path: string[] }> }
+) {
+  return handleRequest(req, "DELETE", props);
 }
