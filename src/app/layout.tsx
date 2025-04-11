7c68606e526a42a4463bf9d1f4a8ac8a4a0c7104
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-11 16:03:56 +0900

    세로로 스크롤되는 현상 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="antialiased">
+      <body className="overflow-y-hidden antialiased">
         {children}
         <SpeedInsights />
         <Analytics />
