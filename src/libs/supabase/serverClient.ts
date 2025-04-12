4ef1836162a27840cf7c66f0d6175f4fea8c2051
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-13 02:37:58 +0900

    Supabase client 생성 파일에 Database 타입 적용

--- src/libs/supabase/serverClient.ts
+++ src/libs/supabase/serverClient.ts
@@ -1,12 +1,14 @@
 "use server";
 
+import { type Database } from "@/types/database";
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export async function createClient() {
   const cookieStore = await cookies();
 
-  return createServerClient(
+  return createServerClient<Database>(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
