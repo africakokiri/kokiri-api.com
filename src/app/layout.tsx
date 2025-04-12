422561c327a4b11f4b7234d9b61c25fc24fac936
Choi Chan Hwan <africakokiri@gmail.com>
2025-04-12 11:55:59 +0900

    Metadata 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,33 @@ export const metadata: Metadata = {
   title: "Kokiri API",
   description:
     "Create a mock REST API. Customize response data for success and error cases.",
+  keywords: [
+    "Next.js",
+    "Kokiri API",
+    "Kokiri",
+    "API",
+    "REST API",
+    "Mock server",
+    "Mock rest api server",
+    "Customize REST API",
+    "Customize response API data"
+  ],
+  openGraph: {
+    title: "Kokiri API",
+    description:
+      "Create a mock REST API. Customize response data for success and error cases.",
+    url: "https://www.kokiri-api.com",
+    siteName: "kokiri-api.com",
+    images: [
+      {
+        url: "/icons/kokiri.svg",
+        width: 1200,
+        height: 630
+      }
+    ],
+    locale: "en_US",
+    type: "website"
+  },
   icons: {
     icon: "/icons/elephant.png"
   }
